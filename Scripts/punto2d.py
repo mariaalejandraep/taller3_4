02ed6ac3b97392e143aa6b28f5a6e8487ba598e8
Juan David <jd.serranom@uniandes.edu.co>
2019-04-07 11:09:02 -0500

    solucion

--- Scripts/punto2d.py
+++ Scripts/punto2d.py
@@ -165,39 +165,40 @@ distanciaCuadricula=0.2 #Distancias entre cuadriculas de grafo
 
 
 
-
+casillasRRT = []
 
 n=int(10/distanciaCuadricula) #Acordarse que 10 debe ser divisible por distanciaCuadricula
 
 
 # Se crea una clase tipo casilla para cada casilla de division del mapa
 class Casilla:
-    def __init__(self, xP, yP, pE,valor):
+    def __init__(self, xP, yP):
         self.x=float(xP)
         self.y=float(yP)
-        self .libre=pE
-        self .numero=valor
+
+
+
+
+
 
 
 #En este metodo se inicializa el nodo, se suscribe a los topicos necesarios, se crea la variable
 #para publicar al topico de motorsVel y tambien se lanza el nodo encargado de graficar.
 def punto2_a():
-    global twistInfoDim, twistInfoPos, MUEVETE, Linksx,Linksy,Obsx,Obsy
+    global twistInfoDim, twistInfoPos, MUEVETE, posInicial,posFinal
     rospy.init_node('punto2_c', anonymous=True)
-
     #Se subscribe a los siguientes topicos
     rospy.Subscriber('InfoObs0', Twist, setObst)#Topico que contiene informacion sobre obstaculo 1
     rospy.Subscriber('pioneerPosition', Twist, setPositionCallback) # Se subscribe al topico de posicion del robot
     rospy.Subscriber('simulationTime', Float32, actualizar)
     time.sleep(0.2) # Espera a que se actualice informacion de todos los obstaculos
-
     #SE publica informacion en los siguientes topicos
+    RRT (posInicial[0], posInicial[1], posFinal[0],  posFinal[1])
     pubMot = rospy.Publisher('motorsVel', Float32MultiArray, queue_size=10)#Se publican velocidades de los motores.
-    pubLibresx = rospy.Publisher('Libresx', Float32MultiArray,  queue_size=10)#Se publican velocidades de los motores.
-    pubLibresy = rospy.Publisher('Libresy', Float32MultiArray,  queue_size=10)#Se publican velocidades de los motores.
-    pubObsx = rospy.Publisher('Obsx', Float32MultiArray,  queue_size=10)#Se publican velocidades de los motores.
-    pubObsy = rospy.Publisher('Obsy', Float32MultiArray,  queue_size=10)#Se publican velocidades de los motores.
-
+    #pubLibresx = rospy.Publisher('Libresx', Float32MultiArray,  queue_size=10)#Se publican velocidades de los motores.
+    # pubLibresy = rospy.Publisher('Libresy', Float32MultiArray,  queue_size=10)#Se publican velocidades de los motores.
+    # pubObsx = rospy.Publisher('Obsx', Float32MultiArray,  queue_size=10)#Se publican velocidades de los motores.
+    # pubObsy = rospy.Publisher('Obsy', Float32MultiArray,  queue_size=10)#Se publican velocidades de los motores.
     #SE inicializa el nodo que grafica en tiempo real
     #package = 'taller2_4'
     #script = 'graficador_punto_3.py'
@@ -206,19 +207,11 @@ def punto2_a():
     #launch.start()
     #process = launch.launch(node)
 
-
-
-
     rate = rospy.Rate(20)
     while not rospy.is_shutdown():
         pubMot.publish(mot)
-        pubLibresx.publish(Linksx)
-        pubLibresy.publish(Linksy)
-        pubObsx.publish(Obsx)
-        pubObsy.publish(Obsy)
-
-
         rate.sleep()
+
 
 
 
@@ -260,15 +253,10 @@ def setPositionCallback(pos):
         posInicial=[twistInfo.linear.x, twistInfo.linear.y, twistInfo.angular.z]
         contador_Inicial=contador_Inicial+1
     elif contador_Inicial<3:
-        creadorMatriz()
-        contador_Inicial=contador_Inicial+1
 
+        contador_Inicial=contador_Inicial+1
     posActual = [twistInfo.linear.x, twistInfo.linear.y, twistInfo.angular.z]
     twistInfo = pos
-
-
-
-
 
 
 #METODOS DE TALLER PASADO
@@ -278,13 +266,10 @@ def calcularVelocidades():
     global v,w,p,kp,ka,kb
     calcularDistancia()
     calcularAngulos()
-
     v = kp*p
     w = ka*a+kb*b
-
     #rospy.loginfo(" v {}".format(v))
     #rospy.loginfo(" w {}".format(w))
-
     mot.data[0] = (v-l*w)/radioRueda
     mot.data[1] =(v+l*w)/radioRueda
 
@@ -293,7 +278,6 @@ def calcularVelocidades():
 #del codigo que se llego al punto final.
 def calcularDistancia():
     global p,dx,dy,umbral,camino,Equivalente,contador_path,n,umbralP
-
     if umbral and contador_path<=(len(camino)-2):
         columna = np.mod(camino[contador_path],n)
         fila = int(np.floor(camino[contador_path]/n))
@@ -311,69 +295,34 @@ def calcularDistancia():
         contador_path=contador_path+1
         umbralP=0.05
         umbral = False
-
     dx = posFinal[0]-posActual[0]
     dy = posFinal[1]-posActual[1]
     p = math.sqrt((dx)**2 + (dy)**2)
-
     if p <= umbralP:
         umbral = True
+
+
 #En este metodo se calculan los angulos a (alpha) y b (beta). Si el umbral se cumplio, las variables p (rho)
 #y a (alpha) se igualan a 0 para permitirle al robot girar y lograr orientarse de manera correcta.
 def calcularAngulos():
     global p,a,b,t,umbral
-
     t = math.atan2(dy,dx)
-
     if not umbral:
         a = -posActual[2] + t
     else:
         p = 0
         a = 0
     b = posActual[2]-posFinal[2] - a
-
-    if (b>math.pi):
+    if b>math.pi:
         b = b-2*math.pi
-    elif (b<-math.pi):
+    elif b<-math.pi:
         b = 2+math.pi+b
-
-
-
-
-
-
-
-
 
 
 #METODOS PARA ESTE TALLER
 
-def creadorMatriz():
-    global distanciaCuadricula, Equivalente,numeroCasillas, n, Links, g, Nodo_Inicial,Nodo_Final
-    xInic=-5.0+np.divide(float(distanciaCuadricula),2.0)
-    yInic=5.0-np.divide(float(distanciaCuadricula),2.0)
-    distVerdaderaA=float('Inf')
-    distVerdaderaB=float('Inf')
-    for i in range(0,n*n):
-        g.add_node(i)
-        mod=np.mod(i,n)#columnas de la matriz
-        div=int(np.floor(i/n))#filas de la matriz
-        if len(Equivalente)<div+1:
-            Equivalente.append([])
-        coordenadax=xInic+mod*distanciaCuadricula
-        coordenaday=yInic-div*distanciaCuadricula
-        nuevaCasilla = Casilla(coordenadax,coordenaday, conexion(coordenadax,coordenaday),i)
-        distanciaACasilla = math.sqrt(np.power(coordenadax-posInicial[0],2)+np.power(coordenaday-posInicial[1],2))
-        distanciaBCasilla = math.sqrt(np.power(coordenadax-posFinal[0],2)+np.power(coordenaday-posFinal[1],2))
-        if distVerdaderaA>distanciaACasilla:
-            Nodo_Inicial=i
-            distVerdaderaA=distanciaACasilla
-        if distVerdaderaB>distanciaBCasilla:
-            Nodo_Final=i
-            distVerdaderaB=distanciaBCasilla
-        Equivalente[div].append(nuevaCasilla)
-    visualizacionGrafica()
-    Graficador_network(Nodo_Inicial,Nodo_Final)
+
+
 
 
 def heuristica(i,j): #Metodo calcula heuristica retorna distancia entre nodos
@@ -383,6 +332,7 @@ def heuristica(i,j): #Metodo calcula heuristica retorna distancia entre nodos
     modj=np.mod(j,n) #columna de nodo j
     distancia=math.sqrt(np.power((Equivalente[divi][modi].x-Equivalente[divj][modj].x),2)+np.power((Equivalente[divi][modi].y-Equivalente[divj][modj].y),2))
     return distancia
+
 
 #Metodo que define la red sobre la cual se trabajara
 def Graficador_network(nodoInicial,nodoFinal):
@@ -423,38 +373,49 @@ def conexion(xCas,yCas):#como variables entran dos numeros relacionados con cada
 
 
 
-def visualizacionGrafica():
-    global n, xLibres,yLibres,xOcupadas,yOcupadas,doIt, Linksx,Linksy,Obsx,Obsy
-    xLibres = []
-    yLibres = []
-    xOcupadas = []
-    yOcupadas = []
-    matriz_obs_links=[]
-    matriz_obs_links.append([])
-    matriz_obs_links.append([])
-    matriz_obs_links.append([])
-    matriz_obs_links.append([])
-    for i in range(0, n**2):
-        if Equivalente[i//n][i%n].libre:
-            xLibres.append(Equivalente[i//n][i%n].x)
-            yLibres.append (Equivalente[i // n][i % n].y)
-            matriz_obs_links[0].append(Equivalente[i//n][i%n].x)
-            matriz_obs_links[1].append(Equivalente[i // n][i % n].y)
 
-        else:
-            xOcupadas.append (Equivalente[i // n][i % n].x)
-            yOcupadas.append (Equivalente[i // n][i % n].y)
-            matriz_obs_links[2].append(Equivalente[i // n][i % n].x)
-            matriz_obs_links[3].append(Equivalente[i // n][i % n].y)
+def RRT(xIni, yIni, xFin, yFin):
+    global g, distanciaCuadricula,camino,MUEVETE,nodoInicial,nodoFinal
+    g.add_node(0)
+    step = distanciaCuadricula
+    radioError = 5
+    casillasRRT.append(Casilla(xIni,yIni))
+    contador = 1
+    llego = False
+    while not llego and contador != 50000:
+        xAleatorio = random.randrange(-500,500,1)/100
+        yAleatorio = random.randrange(-500,500,1)/100
+        posCasillaCercana = posCasillaMasCercana(xAleatorio, yAleatorio)
+        xCercana = casillasRRT[posCasillaCercana].x
+        yCercana = casillasRRT[posCasillaCercana].y
+        teta = math.atan2((yAleatorio-yCercana),(xAleatorio-xCercana))
+        xPropuesto = xCercana + step * math.cos(teta)
+        yPropuesto = yCercana + step * math.sin(teta)
+        if conexion(xPropuesto, yPropuesto):
+            g.add_node(contador)
+            casillasRRT.append(Casilla(xPropuesto, yPropuesto))
+            g.add_edge(posCasillaCercana,contador)
+            contador = contador + 1
+            if math.sqrt((xFin-xPropuesto)**2+(yFin-yPropuesto)**2)<=radioError:
+                llego = True
+        if contador%100==0:
+            print contador
+    camino=A_estrella(0,len(casillasRRT)-1,g)
+    MUEVETE=2
 
 
-    Linksx.data=xLibres
-    Linksy.data=yLibres
-    Obsx.data=xOcupadas
-    Obsy.data=yOcupadas
-    print
 
-
+def posCasillaMasCercana(x, y):
+    global casillasRRT
+    distancia = float('Inf')
+    numCasilla = -1
+    for i in range(0, len(casillasRRT)):
+        casillaActual = casillasRRT[i]
+        distCasillaActual = math.sqrt((casillaActual.x-x)**2+(casillaActual.y-y)**2)
+        if distCasillaActual<distancia:
+            numCasilla = i
+            distancia = distCasillaActual
+    return numCasilla
 
 
 
@@ -478,4 +439,5 @@ if __name__ == '__main__':
 
     except rospy.ROSInterruptException:
         pass
+
 
